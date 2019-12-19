fd4a8ae9b202a7c88717457f565ba87e3ba981ec
kazuki yamshita <ujiuji622@gmail.com>
2019-12-20 02:30:20 +0900

    clean

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,9 @@ void main() => runApp(MainApp());
 
 class MainApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      title: 'TodoList App2',
-      debugShowCheckedModeBanner: false,
-      home: HomePage(key: homePageKey),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'TodoList App2',
+        debugShowCheckedModeBanner: false,
+        home: HomePage(key: homePageKey),
+      );
 }
